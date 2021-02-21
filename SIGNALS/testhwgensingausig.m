08ed381658751010537fcf863ce85e9bb2c5f507
iphysresearch <hewang@mail.bnu.edu.cn>
2021-02-21 15:19:16 +0800

    l3lab - sine-Gaussian signal

---
+++ SIGNALS/testhwgensingausig.m
@@ -0,0 +1,52 @@
+%% Plot the sine-Gaussian signal
+% Signal parameters
+A = 10;
+f0 = 50;
+phi0 = pi;
+t0 = 0.6;
+sigma = 0.1;
+% Instantaneous frequency after 1 sec is 
+samplFreq = 5*maxFreq;
+samplIntrvl = 1/samplFreq;
+
+% Time samples
+timeVec = 0:samplIntrvl:1.0;
+% Number of samples
+nSamples = length(timeVec);
+
+% Generate the signal
+sigVec = hwgensingausig(timeVec,A,f0,phi0,t0,sigma);
+
+%Plot the signal 
+figure;
+plot(timeVec,sigVec,'Marker','.','MarkerSize',24);
+
+%Plot the periodogram
+%--------------
+%Length of data 
+dataLen = timeVec(end)-timeVec(1);
+%DFT sample corresponding to Nyquist frequency
+kNyq = floor(nSamples/2)+1;
+% Positive Fourier frequencies
+posFreq = (0:(kNyq-1))*(1/dataLen);
+% FFT of signal
+fftSig = fft(sigVec);
+% Discard negative frequencies
+fftSig = fftSig(1:kNyq);
+
+%Plot periodogram
+figure;
+plot(posFreq,abs(fftSig));
+
+%Plot a spectrogram
+%----------------
+winLen = 0.2;%sec
+ovrlp = 0.1;%sec
+%Convert to integer number of samples 
+winLenSmpls = floor(winLen*samplFreq);
+ovrlpSmpls = floor(ovrlp*samplFreq);
+[S,F,T]=spectrogram(sigVec,winLenSmpls,ovrlpSmpls,[],samplFreq);
+figure;
+imagesc(T,F,abs(S)); axis xy;
+xlabel('Time (sec)');
+ylabel('Frequency (Hz)');

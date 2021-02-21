2a443da3a7d387a40c2dd0c81b27de59906c3a4e
iphysresearch <hewang@mail.bnu.edu.cn>
2021-02-21 14:59:10 +0800

    l3lab - linear chirp signal

---
+++ SIGNALS/hwgenlcsig.m
@@ -0,0 +1,12 @@
+function sigVec = hwgenlcsig(dataX,snr,f0,f1,phi0)
+% Generate a linear chirp signal
+% S = HWGENLCSIG(X,SNR,F0,F1,P)
+% Generates a sinusoidal signal S. X is the vector of
+% time stamps at which the samples of the signal are to be computed. SNR is
+% the matched filtering signal-to-noise ratio of S. (F0,F1) and P is the 
+% frequency and phase of the linear chirp signal respectively.
+
+%He Wang, Feb 2021
+
+sigVec = sin(2*pi*(f0*dataX + f1*dataX.^2) + phi0);
+sigVec = snr*sigVec/norm(sigVec);

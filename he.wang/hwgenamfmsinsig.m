279b4dd66c1d676372a46eeb7e601af81240c8ed
iphysresearch <hewang@mail.bnu.edu.cn>
2021-02-22 19:44:10 +0800

    fix for l3lab

--- he.wang/hwgenamfmsinsig.m
+++ he.wang/hwgenamfmsinsig.m
@@ -1,13 +1,13 @@
-function sigVec = hwgenamfmsinsig(dataX,snr,f0,f1,b)
+function sigVec = hwgenamfmsinsig(dataX,snr,f0,f1,f2,b)
 % Generate an AM-FM sinusoid signal
-% S = HWGENAMFMSIG(X,SNR,F0,F1,B)
+% S = HWGENAMFMSIG(X,SNR,F0,F1,F2,B)
 % Generates an AM-FM sinusoid signal S. X is the vector of
 % time stamps at which the samples of the signal are to be computed. SNR is
-% the matched filtering signal-to-noise ratio of S. F1 is the frequency of 
-% the carrier sinusoid and (B, F0) is the (phase, frequency) in the 
-% modulator sinusoid part.
+% the matched filtering signal-to-noise ratio of S. F0 is the frequency of 
+% the carrier sinusoid and (B, F1,F2) is the (phase, frequency, frequency) 
+% in the modulator sinusoid part.
 
 %He Wang, Feb 2021
 
-sigVec = cos(2*pi*f1*dataX) .* sin(2*pi*f0*dataX + b*cos(2*pi*f1*dataX));
+sigVec = cos(2*pi*f2*dataX) .* sin(2*pi*f0*dataX + b*cos(2*pi*f1*dataX));
 sigVec = snr*sigVec/norm(sigVec);

64d03101b59a680512193f8d727390c9df6fac41
iphysresearch <hewang@mail.bnu.edu.cn>
2021-02-23 18:16:46 +0800

    Add codes for local frame <- Analytical forms

---
+++ he.wang/testskyplot.m
@@ -0,0 +1,21 @@
+%% Test harness for FORMULAFP and SKYPLOT
+%Azimuthal angle
+phiVec = 0:0.1:(2*pi);
+%Polar angle
+thetaVec = 0:0.1:pi;
+
+%Function handle: F+ from formula
+fp = @(x,y) formulafp(x,y);
+%Function handle: F+ from formula
+fc = @(x,y) formulafc(x,y);
+
+figure;
+subplot(2,1,1);
+skyplot(phiVec,thetaVec,fp);
+title('F_+');
+axis equal;
+
+subplot(2,1,2);
+skyplot(phiVec,thetaVec,fc);
+title('F_x');
+axis equal;
